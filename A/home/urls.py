ac8d70f298ae71de5a0f27fc0113cd15a2ffa1de
Mobin <mobin.esmaeili00@gmail.com>
2025-05-31 23:24:43 +0330

    add details page for products

--- A/home/urls.py
+++ A/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import ProductDetailView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
 ]
